40b1df6ef89e274ae8ca1e719650542b60618778
Paul Frank <pail@gmx.net>
2023-06-05 11:23:24 +0000

    2 column layout for larger screens

--- src/components/EnergyAssistant.vue
+++ src/components/EnergyAssistant.vue
@@ -1,65 +1,67 @@
 <template>
-    <div class="h-full v-full bg-base-200 p-4 flex justify-center ">
+    <div class="h-full v-full bg-base-200 p-4 flex justify-center min-h-screen">
         <div v-if="connected">
-            <PowerFlowCard :home_consumption_power=home.power.home_consumption :solar_power=home.power.solar_production>
-            </PowerFlowCard>
+            <div class="grid grid-cols-1 lg:grid-cols-2">
+                <PowerFlowCard :home_consumption_power=home.power.home_consumption :solar_power=home.power.solar_production>
+                </PowerFlowCard>
 
-            <div class="card w-80 lg:w-96 bg-base-100 shadow-xl m-4 p-4">
-                <div class="card-body  w-full">
-                    <div class="grid grid-cols-6">
-                        <div>
-                            <span class="mdi text-3xl mdi-home"></span>
-                        </div>
-                        <span class="text-left font-bold text-lg col-span-5">{{ home.name }}
-                        </span>
-                        <div class="flex items-end">
-                            <span v-if="expanded" @click="expanded = !expanded"
-                                className="mdi mdi-arrow-up-drop-circle-outline" />
-                            <span v-else @click="expanded = !expanded" className="mdi mdi-arrow-down-drop-circle-outline" />
-                        </div>
-                        <div class="items-center col-span-5">
-                            <div class="mb-4">
-                                <div class="italic">Leistung</div>
-                                <p class="text-left">Solarproduktion<span class="float-right">{{
-                                    home.power.solar_production }} W</span></p>
+                <div class="card w-80 lg:w-96 bg-base-100 shadow-xl m-4 p-4">
+                    <div class="card-body  w-full">
+                        <div class="grid grid-cols-6">
+                            <div>
+                                <span class="mdi text-3xl mdi-home"></span>
                             </div>
-                            <div class="italic">Aktueller Verbrauch</div>
-                            <div class="mt-2 mb-4">
-                                <SelfSufficiencyBar :self_sufficiency=home.power.self_sufficiency
-                                    :consumed_energy=home.power.home_consumption
-                                    :consumed_solar_energy=solar_consumption_power unit="W"></SelfSufficiencyBar>
+                            <span class="text-left font-bold text-lg col-span-5">{{ home.name }}
+                            </span>
+                            <div class="flex items-end">
+                                <span v-if="expanded" @click="expanded = !expanded"
+                                    className="mdi mdi-arrow-up-drop-circle-outline" />
+                                <span v-else @click="expanded = !expanded"
+                                    className="mdi mdi-arrow-down-drop-circle-outline" />
                             </div>
-                            <div v-show="expanded">
-                                <div class="italic">Verbrauch heute:</div>
-
-                                <div class="mt-2 mb-4">
-                                    <SelfSufficiencyBar :self_sufficiency=home.today.self_sufficiency
-                                        :consumed_energy=home.today.consumed_energy
-                                        :consumed_solar_energy=home.today.consumed_solar_energy></SelfSufficiencyBar>
+                            <div class="items-center col-span-5">
+                                <div class="mb-4">
+                                    <div class="italic">Leistung</div>
+                                    <p class="text-left">Solarproduktion<span class="float-right">{{
+                                        home.power.solar_production }} W</span></p>
                                 </div>
+                                <div class="italic">Aktueller Verbrauch</div>
+                                <div class="mt-2 mb-4">
+                                    <SelfSufficiencyBar :self_sufficiency=home.power.self_sufficiency
+                                        :consumed_energy=home.power.home_consumption
+                                        :consumed_solar_energy=solar_consumption_power unit="W"></SelfSufficiencyBar>
+                                </div>
+                                <div v-show="expanded">
+                                    <div class="italic">Verbrauch heute:</div>
 
-                                <div class="italic">Gesamtverbrauch:</div>
-                                <p class="text-left">Solarenergie: <span class="float-right">{{
-                                    home.overall.consumed_solar_energy.toFixed(2) }} kWh</span></p>
-                                <p class="text-left">Gesamtverbrauch: <span class="float-right">{{
-                                    home.overall.consumed_energy.toFixed(2) }} kWh</span></p>
+                                    <div class="mt-2 mb-4">
+                                        <SelfSufficiencyBar :self_sufficiency=home.today.self_sufficiency
+                                            :consumed_energy=home.today.consumed_energy
+                                            :consumed_solar_energy=home.today.consumed_solar_energy></SelfSufficiencyBar>
+                                    </div>
+
+                                    <div class="italic">Gesamtverbrauch:</div>
+                                    <p class="text-left">Solarenergie: <span class="float-right">{{
+                                        home.overall.consumed_solar_energy.toFixed(2) }} kWh</span></p>
+                                    <p class="text-left">Gesamtverbrauch: <span class="float-right">{{
+                                        home.overall.consumed_energy.toFixed(2) }} kWh</span></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div v-for="(device, index) in home.devices" :key="index">
-                <DeviceCard :name=device.name :icon=device.icon :power=device.power :overall=device.overall
-                    :today=device.today>
-                </DeviceCard>
+                <div v-for="(device, index) in home.devices" :key="index">
+                    <DeviceCard :name=device.name :icon=device.icon :power=device.power :overall=device.overall
+                        :today=device.today>
+                    </DeviceCard>
+                </div>
+                <div v-for="(device, index) in home.heat_pumps" :key="index">
+                    <HeatPumpCard :name=device.name :icon=device.icon :power=device.power :overall=device.overall
+                        :today=device.today :actual_temperature=device.actual_temperature>
+                    </HeatPumpCard>
+                </div>
             </div>
-            <div v-for="(device, index) in home.heat_pumps" :key="index">
-                <HeatPumpCard :name=device.name :icon=device.icon :power=device.power :overall=device.overall
-                    :today=device.today :actual_temperature=device.actual_temperature>
-                </HeatPumpCard>
-            </div>
-
         </div>
         <div v-else class="alert alert-error">
             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
