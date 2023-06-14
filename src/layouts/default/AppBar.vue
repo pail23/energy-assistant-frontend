47a1b630f49cba8fbc2001782501fecc039954e4
Paul Frank <pail@gmx.net>
2023-06-14 20:39:59 +0000

    add router again

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar bg-base-100 ">
+    <div>
+      <a class="btn btn-ghost normal-case text-xl">Energy Assistant Stadel 15</a>
+    </div>
+
+    <div class="flex-1">
+      <div class="join mx-auto">
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Live" checked />
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Heute" />
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Woche" />
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Monat" />
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Jahr" />
+        <input class="btn join-item w-16" type="radio" name="options" aria-label="Gesamt" />
+      </div>
+    </div>
+
+    <div class="flex-none">
+      <select data-choose-theme class="select select-ghost w-full max-w-xs">
+        <option value="">Light</option>
+        <option value="dark">Dark</option>
+        <option value="bumblebee">bumblebee</option>
+      </select>
+    </div>
+
+
+  </div>
+</template>
+  
+<script lang="ts" setup>
+
+</script>
