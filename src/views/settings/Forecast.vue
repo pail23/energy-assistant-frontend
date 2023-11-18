a5600436477b8b21243f2f33c937f1b0712a70b2
Paul Frank <pail@gmx.net>
2023-11-18 16:33:36 +0000

    add progress indicator

--- src/views/settings/Forecast.vue
+++ src/views/settings/Forecast.vue
@@ -3,7 +3,13 @@
     <v-card-title>Create forecast model</v-card-title>
     <v-card-item>
       Create the load forcast model based on historical data.
+      <v-progress-linear
+        v-if="isCreating"
+        indeterminate
+        class="m-2"
+      ></v-progress-linear>
     </v-card-item>
+
     <v-card-actions>
       <v-btn class="m-2" @click="CreateModel">
         Create load forecast model
@@ -16,6 +22,11 @@
     <v-card-item>
       Tune the load forcast model in order to optimize the forecast quality.
       This might take some time.
+      <v-progress-linear
+        v-if="isTuning"
+        indeterminate
+        class="m-2"
+      ></v-progress-linear>
     </v-card-item>
     <v-card-actions>
       <v-btn class="m-2" @click="TuneModel"> Tune load forecast model </v-btn>
@@ -25,14 +36,20 @@
 
 <script lang="ts" setup>
 import { api } from '@/api/energyAssistant.api';
+import { ref } from 'vue';
+
+const isCreating = ref(false);
+const isTuning = ref(false);
 
 const CreateModel = async function () {
+  isCreating.value = true;
   let model = await api.createModel();
-  alert('model created:' + model.data.model);
+  isCreating.value = false;
 };
 
 const TuneModel = async function () {
+  isTuning.value = true;
   let model = await api.tuneModel();
-  alert('model tuned:' + model.data.model);
+  isTuning.value = false;
 };
 </script>
